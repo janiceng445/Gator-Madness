a22f6b35f658140560a2ec48ff93a7c815895b02
janiceng445 <janiceng@ufl.edu>
2019-05-06 16:45:38 -0400

    Movement implemented

--- Gator Madness/Assets/Scripts/PlayerController.cs
+++ Gator Madness/Assets/Scripts/PlayerController.cs
@@ -4,29 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject player;
-    public Rigidbody2D m_Rigidbody2D; 
-    public float speed = 50f;
-    float horizontalMove = 0f; 
+    public float speed;
+    private Rigidbody2D rb2d;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * speed; 
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
     }
-    void FixedUpdate()
-    {
-        Move(horizontalMove * Time.fixedDeltaTime);
-    }
-    public void Move(float move)
-    {
-        Vector3 targetVelocity = new Vector2(move + 10f, m_Rigidbody2D.velocity.y);
-        m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
-    }
+
 }   
