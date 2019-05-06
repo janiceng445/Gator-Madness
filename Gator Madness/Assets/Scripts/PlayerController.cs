311e7b378d0881ad8f442aa8525b77d0ac6d6ef7
ijagonng <50304385+ijagonng@users.noreply.github.com>
2019-05-06 02:47:10 -0400

    Coding Movement
    move left and right

---
+++ Gator Madness/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D m_Rigidbody2D; 
+    public float speed = 50f;
+    float horizontalMove = 0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed; 
+    }
+    void FixedUpdate()
+    {
+        Move(horizontalMove * Time.fixedDeltaTime);
+    }
+    public void Move(float move)
+    {
+        Vector3 targetVelocity = new Vector2(move + 10f, m_Rigidbody2D.velocity.y);
+    }
+}
