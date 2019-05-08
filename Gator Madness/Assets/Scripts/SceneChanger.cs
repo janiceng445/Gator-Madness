b843fbebd03479794fcb3a9f9c4f11f0ffec805d
ijagonng <50304385+ijagonng@users.noreply.github.com>
2019-05-08 02:40:34 -0400

    FadeIn and FadeOut Scenes
    Transition between PlayScene and TitleScene using Fade Black Transition Image

---
+++ Gator Madness/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadetoLevel(1);
+        }
+    }
+
+    public void FadetoLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void onFadeComplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
